e09dcdb83ee004c46080db85b6f0bec64db6f147
Elderkly <897676943@qq.com>
2021-05-24 20:49:56 +0800

    从相册获取图片

--- app/src/main/java/com/example/text_extraction/MainActivity.java
+++ app/src/main/java/com/example/text_extraction/MainActivity.java
@@ -1,14 +1,215 @@
 package com.example.text_extraction;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.lifecycle.AndroidViewModel;
 
+import android.Manifest;
+import android.content.ContentUris;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
+    private static final int TAKE_PHOTO=1;
+    private static final int CHOOSE_PHOTO=2;
+    private Uri imageUri;
+    private ImageView imageView;
+//    private CircleImageView circleImageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.addListener();
+    }
+
+    public void addListener() {
+        Button PZ = (Button)findViewById(R.id.button1);
+        Button TP = (Button)findViewById(R.id.button2);
+        PZ.setOnClickListener(v -> {
+            System.out.println("123");
+            File file = new File(getExternalCacheDir(), "output_image.jpg");
+            try {
+                if (file.exists()) {
+                    file.delete();
+                }
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (Build.VERSION.SDK_INT >= 24) {
+                imageUri = FileProvider.getUriForFile(com.example.text_extraction.MainActivity.this, "fileProvider", file);
+            } else {
+                imageUri = Uri.fromFile(file);
+            }
+
+            if (ContextCompat.checkSelfPermission(v.getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(com.example.text_extraction.MainActivity.this, new String[]{Manifest.permission.CAMERA}, 1);
+            } else {
+                callCamera();
+            }
+        });
+
+        TP.setOnClickListener(v -> {
+            System.out.println("456");
+            if (ContextCompat.checkSelfPermission(v.getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(com.example.text_extraction.MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
+            } else {
+                goPhotoAlbum();
+            }
+        });
+    }
+
+    //打开相机
+    private void callCamera() {
+        //启动相机权限
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, TAKE_PHOTO);
+    }
+
+    //激活相册操作
+    private void goPhotoAlbum() {
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent, CHOOSE_PHOTO);
+    }
+
+    //  拒绝权限
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String [] permissions,int[] grantResults){
+        switch (requestCode){
+            case 1:
+                if (grantResults.length>0&&grantResults[0]!=PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,"拒绝权限将无法正常使用",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case TAKE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    try {
+                        String filePath = new File(getCacheDir(), "take_photo1.jpg").getPath();
+                        ExifInterface exifInterface = new ExifInterface(filePath);
+                        int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL);
+                        Log.i(TAG, "onActivityResult: orientation " + orientation);
+
+                        Bitmap bitmap = BitmapFactory.decodeStream(getContentResolver().openInputStream(imageUri));
+                        Matrix matrix = new Matrix();
+                        matrix.postRotate(90);
+                        Bitmap bitmap1 = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+                        System.out.println("****"+bitmap1);
+                        //                        imageView.setImageBitmap(bitmap);
+//                        circleImageView.setImageBitmap(bitmap);
+//                        imageView.setImageBitmap(bitmap1);
+//                        circleImageView.setImageBitmap(bitmap1);
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK) {
+                    if (Build.VERSION.SDK_INT >= 19) {
+                        handleImageNow(data);
+                    } else {
+                        handleImageBefore(data);
+                    }
+                }
+                break;
+        }
+    }
+
+    public void handleImageBefore(Intent data){
+        Uri uri=data.getData();
+        String imagePath=getImagePath(uri,null);
+        displayImage(imagePath);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void handleImageNow(Intent data){
+        String imagePath=null;
+        Uri uri=data.getData();
+        if (DocumentsContract.isDocumentUri(this,uri)){
+            String docId=DocumentsContract.getDocumentId(uri);
+            if ("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1];
+                String selection=MediaStore.Images.Media._ID+"="+id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri= ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath = getImagePath(contentUri,null);
+            }
+        }else if ("content".equalsIgnoreCase(uri.getScheme())){
+            imagePath = getImagePath(uri,null);
+        }else if ("file".equalsIgnoreCase(uri.getScheme())){
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);
+
+        //直接用图库图片封装过的Uri也可以加载图片
+//        displayImage(uri);
+    }
+
+    public String getImagePath(Uri uri,String selection){
+        String path=null;
+        Cursor cursor=getContentResolver().query(uri,null,selection,null,null);
+        if (cursor!=null){
+            if (cursor.moveToFirst()){
+                path=cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+
+    public void displayImage(String imagePath){
+        if (imagePath!=null){
+            Bitmap bitmap=BitmapFactory.decodeFile(imagePath);
+            System.out.println("****"+bitmap);
+//            imageView.setImageBitmap(bitmap);
+//            circleImageView.setImageBitmap(bitmap);
+        }else {
+            Log.i(TAG, "displayImage: 图片路径存在问题");
+        }
     }
 }
